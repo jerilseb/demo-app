e705c36c9737991b8e02e4518dc53ac3d3c7a2bb
Jeril Sebastian <seb988@gmail.com>
2026-01-02 19:31:56 +0530

    some updates

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     groups.forEach(group => {
         setupGroupPlayback(group);
     });
+
+    setupImageModal();
 });
 
 function setupGroupPlayback(groupElement) {
@@ -18,5 +20,29 @@ function setupGroupPlayback(groupElement) {
                 }
             });
         });
+    });
+}
+
+function setupImageModal() {
+    const modal = document.getElementById('image-modal');
+    const modalImg = document.getElementById('modal-img');
+    const closeBtn = document.querySelector('.close');
+    const refImages = document.querySelectorAll('.ref-img');
+
+    refImages.forEach(img => {
+        img.addEventListener('click', function() {
+            modal.style.display = 'block';
+            modalImg.src = this.src;
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.style.display = 'none';
+        }
     });
 }
